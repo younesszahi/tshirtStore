bd6d9d560558816d5e7f97cb884a72922c295b4a
younes zahi <younesz4hi@gmail.com>
2025-05-12 20:09:22 +0200

    change the banner to something else

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -12,13 +12,13 @@ const Banner = () => {
       />
       <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
         <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px]">
-          Level Up Your Gaming Experience
+          Art You Can Wear
         </h2>
         <p className="max-w-[343px] font-medium text-gray-800/60">
-          From immersive sound to precise controls—everything you need to win
+          Original designs that speak louder than words.
         </p>
         <button className="group flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">
-          Buy now
+          Discover Your Look
           <Image
             className="group-hover:translate-x-1 transition"
             src={assets.arrow_icon_white}
